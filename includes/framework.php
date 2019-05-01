64174e322ba8423a4f296760e4af78483a321555
Puleeno Nguyen <puleeno@gmail.com>
2019-05-01 19:28:02 +0700

    add framework

--- includes/framework.php
+++ includes/framework.php
@@ -1,13 +1,18 @@
 <?php
+/**
+ * Jankx Framework
+ */
 
-$template_directory = realpath(dirname(__FILE__) . '/..');
+$template_directory = realpath( dirname( __FILE__ ) . '/..' );
 
 /**
  * Check composer package is installed
  */
-$composer = sprintf('%s/vendor/autoload.php', $template_directory);
-if (file_exists($composer)) {
+$composer = sprintf( '%s/vendor/autoload.php', $template_directory );
+if ( file_exists( $composer ) ) {
     require_once $composer;
 } else {
-    exit(__('Please ensure composer packages are installed', 'jankx'));
+    if ( ! is_admin() ) {
+        exit( __( 'Please ensure composer packages are installed', 'jankx' ) );
+    }
 }
