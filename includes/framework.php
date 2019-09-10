bf67240de77fd49090710d3b19158e74e3e4f07f
Puleeno Nguyen <puleeno@gmail.com>
2019-09-11 02:46:48 +0700

    Fix coding standard

--- includes/framework.php
+++ includes/framework.php
@@ -10,9 +10,9 @@ $template_directory = realpath( dirname( __FILE__ ) . '/..' );
  */
 $composer = sprintf( '%s/vendor/autoload.php', $template_directory );
 if ( file_exists( $composer ) ) {
-    require_once $composer;
+	require_once $composer;
 } else {
-    if ( ! is_admin() ) {
-        exit( __( 'Please ensure composer packages are installed', 'jankx' ) );
-    }
+	if ( ! is_admin() ) {
+		exit( __( 'Please ensure composer packages are installed', 'jankx' ) );
+	}
 }
