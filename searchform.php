fbbc97b5a027a86fc26b981c01c1db56346d7f3b
Puleeno Nguyen <puleeno@gmail.com>
2019-09-11 02:44:32 +0700

    Load searchform via Jankx template

--- searchform.php
+++ searchform.php
@@ -1,7 +1,23 @@
-<form action="<?php echo site_url(); ?>" class="form-inline my-2 my-lg-0">
-	<div class="from-wrap">
-		<input type="hidden" name="post_type" value="product" />
-		<input type="text" name="s" value="<?php echo get_search_query(); ?>" />
-		<button>Tìm kiếm</button>
-	</div>
-</form>
+<?php
+/**
+ * This is a part of Jankx Framework
+ *
+ * PLEASE DON'T MODIFY THIS FILE
+ *
+ * @package Jankx/Template
+ * @author Puleeno Nguyen <puleeno@gmail.com>
+ * @since 1.0
+ */
+
+/**
+ * Load searchform template via Jankx Template Engine
+ *
+ * Site header can be modified via feature override templates of WordPress
+ * so please don't modify this file to ensure Jankx Framework working is exactly.
+ */
+jankx_template(
+	array(
+		'partials/searchform',
+		'searchform',
+	)
+);
