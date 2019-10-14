561a6855239e64ad228ca7319649f8e540175bba
Puleeno Nguyen <puleeno@gmail.com>
2019-10-14 23:34:06 +0700

    Add startup file to setup framework

---
+++ includes/bootstrap/startup.php
@@ -0,0 +1,7 @@
+<?php
+define('JANKX_ABSPATH', realpath(dirname(__FILE__) . '/../..'));
+
+/**
+ * Page Builder plugin integrations
+ */
+require_once JANKX_ABSPATH . '/includes/elementor/class-jankx-elementor.php';
