142e9a1d8f640f56e80f96653a7c08c8cf608efc
Puleeno Nguyen <puleeno@gmail.com>
2019-10-26 20:20:31 +0700

    Update comments template

---
+++ comments.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This is a part of Jankx Framework
+ *
+ * PLEASE DON'T MODIFY THIS FILE
+ *
+ * @package Jankx/Template
+ * @author Puleeno Nguyen <puleeno@gmail.com>
+ * @since 1.0
+ */
+
+/**
+ * Load sidebar template via Jankx Template Engine
+ *
+ * Site sidebar can be modified via feature override templates of WordPress
+ * so please don't modify this file to ensure Jankx Framework working is exactly.
+ */
+jankx_template(
+	array(
+		'partials/comments',
+		'comments',
+	)
+);
