c52140e788fc74e1933889e483ac6f0b975a6ca3
Puleeno Nguyen <puleeno@gmail.com>
2020-04-14 23:05:16 +0700

    Added blank footer template

---
+++ footer-blank.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This is a part of Jankx Framework
+ *
+ * PLEASE DON'T MODIFY THIS FILE
+ *
+ * @package Jankx/Framework
+ * @author Puleeno Nguyen <puleeno@gmail.com>
+ * @since 1.0
+ */
+
+/**
+ * Call the WordPress footer hook
+ * to integrate with other plugins and themes
+ */
+ wp_footer();
+?>
+
+</body>
+</html>
