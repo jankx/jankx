642170a5192aacd83e5db3d2e75bf2311bf98eb6
Puleeno Nguyen <puleeno@gmail.com>
2020-10-20 19:05:55 +0700

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -1,18 +1,10 @@
-<?php
-/**
- * This is a part of Jankx Framework
- *
- * PLEASE DON'T MODIFY THIS FILE
- *
- * @package Jankx/Template
- * @author Puleeno Nguyen <puleeno@gmail.com>
- * @since 1.0.0
- */
-
-/**
- * Load searchform template via Jankx Template Engine
- *
- * Site header can be modified via feature override templates of WordPress
- * so please don't modify this file to ensure Jankx Framework working is exactly.
- */
-jankx_template( 'searchform' );
+<form action="/" method="get" class="quick-search-form">
+    <div class="text-wrap">
+        <span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
+        <input type="text" name="s" id="search" value="<?php the_search_query(); ?>"  placeholder="<?php echo esc_attr(__('Enter your keyword', 'jankx')); ?>" />
+    </div>
+    <button class="search-button">
+        <?php echo esc_html(__('Search')); ?>
+    </button>
+</form>
+<div class="quick-search-content"></div>
