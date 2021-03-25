a81f218dbc4066225a7493b416679342a11b8e41
Puleeno Nguyen <puleeno@gmail.com>
2021-03-25 18:16:10 +0700

    Update app.js

--- assets/src/app.js
+++ assets/src/app.js
@@ -11,3 +11,27 @@ var smoothScroll = new scrollToSmooth( 'a', {
     fixedHeader: null
   })
 smoothScroll.init();
+
+
+
+var breakpoints = 768;
+// When the user scrolls the page, execute jankx_sticky_header_detector
+// @link: https://www.w3schools.com/howto/howto_js_sticky_header.asp
+window.onscroll = function() {jankx_sticky_header_detector()};
+// Get the header
+var header = document.getElementById("jankx-site-header");
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function jankx_sticky_header_detector() {
+  if (window.innerWidth >= breakpoints) {
+    if (window.pageYOffset > sticky) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+  } else {
+    header.classList.remove("sticky");
+  }
+}
