428254e329fe9a5c378afbe45a3f2f748271460c
Puleeno Nguyen <puleeno@gmail.com>
2022-08-18 19:11:06 +0700

    Update app.js to compatibles with full-page layout

--- assets/src/app.js
+++ assets/src/app.js
@@ -21,19 +21,21 @@ var breakpoints = 768;
 window.onscroll = function() {jankx_sticky_header_detector()};
 // Get the header
 var header = document.getElementById("jankx-site-header");
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
+if (header) {
+  // Get the offset position of the navbar
+  var sticky = header.offsetTop;
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function jankx_sticky_header_detector() {
-  if (window.innerWidth >= breakpoints) {
-    if (window.pageYOffset > sticky) {
-      header.classList.add("sticky");
+  // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  function jankx_sticky_header_detector() {
+    if (window.innerWidth >= breakpoints) {
+      if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+      } else {
+        header.classList.remove("sticky");
+      }
     } else {
       header.classList.remove("sticky");
     }
-  } else {
-    header.classList.remove("sticky");
   }
 }
 
