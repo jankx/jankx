2c36d9f7a448b8e14e544aee6821066c65d306e6
Puleeno Nguyen <puleeno@gmail.com>
2022-08-19 09:01:53 +0700

    Fix function jankx_sticky_header_detector is not defined

--- assets/src/app.js
+++ assets/src/app.js
@@ -1,53 +1,55 @@
 var smoothScroll = new scrollToSmooth( 'a.jankx-scrollto', {
-    targetAttribute: 'href',
-    topOnEmptyHash: false,
-    duration: 400,
-    durationRelative: false,
-    durationMin: false,
-    durationMax: false,
-    easing: 'easeOutCubic',
-    onScrollStart: (data) => { console.log(data); },
-    onScrollUpdate: (data) => { console.log(data); },
-    onScrollEnd: (data) => { console.log(data); },
-    fixedHeader: null
-  })
+  targetAttribute: 'href',
+  topOnEmptyHash: false,
+  duration: 400,
+  durationRelative: false,
+  durationMin: false,
+  durationMax: false,
+  easing: 'easeOutCubic',
+  onScrollStart: (data) => { console.log(data); },
+  onScrollUpdate: (data) => { console.log(data); },
+  onScrollEnd: (data) => { console.log(data); },
+  fixedHeader: null
+})
 smoothScroll.init();
 
 
 
+
+// Get the header
+var header = document.getElementById("jankx-site-header");
+if (header) {
 var breakpoints = 768;
 // When the user scrolls the page, execute jankx_sticky_header_detector
 // @link: https://www.w3schools.com/howto/howto_js_sticky_header.asp
 window.onscroll = function() {jankx_sticky_header_detector()};
-// Get the header
-var header = document.getElementById("jankx-site-header");
-if (header) {
-  // Get the offset position of the navbar
-  var sticky = header.offsetTop;
 
-  // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-  function jankx_sticky_header_detector() {
-    if (window.innerWidth >= breakpoints) {
-      if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-      } else {
-        header.classList.remove("sticky");
-      }
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function jankx_sticky_header_detector() {
+  if (window.innerWidth >= breakpoints) {
+    if (window.pageYOffset > sticky) {
+      header.classList.add("sticky");
     } else {
       header.classList.remove("sticky");
     }
+  } else {
+    header.classList.remove("sticky");
   }
+}
 }
 
 var primaryMenuHasSubItems = document.querySelectorAll('.navigation-primary .show-child');
 if (primaryMenuHasSubItems.length > 0) {
-  for (i=0; i<primaryMenuHasSubItems.length; i++) {
-      primaryMenuHasSubItem = primaryMenuHasSubItems[i];
-      primaryMenuHasSubItem.addEventListener('click', function (e) {
-          currentMenuItem = e.target.findParent('.menu-item');
-          if (currentMenuItem) {
-              currentMenuItem.toggleClass('show-sub');
-          }
-      });
-  }
+for (i=0; i<primaryMenuHasSubItems.length; i++) {
+    primaryMenuHasSubItem = primaryMenuHasSubItems[i];
+    primaryMenuHasSubItem.addEventListener('click', function (e) {
+        currentMenuItem = e.target.findParent('.menu-item');
+        if (currentMenuItem) {
+            currentMenuItem.toggleClass('show-sub');
+        }
+    });
+}
 }
