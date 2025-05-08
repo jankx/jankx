0ab7b83b1866f849671872cd73fbadd64e18b0c0
Puleeno Nguyen <puleeno@gmail.com>
2025-05-08 10:02:43 +0700

    fix bug load stylesheet in child theme

--- functions.php
+++ functions.php
@@ -7,12 +7,12 @@ function jankx_register_css_and_scripts()
 {
     $jankxCssDeps = array('jankx-base');
     $stylesheetName = Jankx::theme()->get_stylesheet();
+    // var_dump($stylesheetName);
+    // die;
     $jankxTemplate = wp_get_theme(Jankx::templateStylesheet());
-
     if (is_child_theme() && apply_filters('jankx/styles/includes/main', true)) {
         $stylesheetUri = sprintf('%s/style.css', get_template_directory_uri());
-        $jankxCssDeps[] = $jankxTemplate->get_stylesheet();
-
+        $jankxCssDeps[] = $jankxTemplate->get_template();
         css(
             $jankxTemplate->get_stylesheet(),
             $stylesheetUri,
