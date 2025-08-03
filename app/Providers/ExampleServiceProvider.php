8a1a4f26077092b77f11d6163b4580b32173f7ee
Puleeno Nguyen <puleeno@gmail.com>
2025-08-04 01:12:21 +0700

    test child theme and parent theme are same namespaces

---
+++ app/Providers/ExampleServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Providers;
+
+use Jankx\Foundation\Application;
+
+class ExampleServiceProvider extends \Jankx\Support\Providers\ServiceProvider
+{
+    /**
+     * Register the service provider.
+     */
+    public function register(Application $app)
+    {
+        // Register any application services here.
+    }
+
+    /**
+     * Bootstrap the application events.
+     */
+    public function boot(Application $app)
+    {
+        // Bootstrap any application services here.
+    }
+}
