64aa714ec76230bf33bec58b831ffc06c2c769c9
Puleeno Nguyen <puleeno@gmail.com>
2025-08-07 12:07:36 +0700

    Added theme support and theme settings

--- includes/Jankx/Services/ThemeService.php
+++ includes/Jankx/Services/ThemeService.php
@@ -7,5 +7,37 @@ class ThemeService
     public function initFeatures()
     {
         add_theme_support('menus');
+
+        // Add Gutenberg support
+        add_theme_support('editor-styles');
+        add_theme_support('wp-block-styles');
+        add_theme_support('responsive-embeds');
+        add_theme_support('align-wide');
+
+        // Add support for navigation block
+        add_theme_support('block-nav-menus');
+
+        // Add support for custom logo
+        add_theme_support('custom-logo');
+
+        // Add support for post thumbnails
+        add_theme_support('post-thumbnails');
+
+        // Add support for HTML5 markup
+        add_theme_support('html5', array(
+            'search-form',
+            'comment-form',
+            'comment-list',
+            'gallery',
+            'caption',
+            'style',
+            'script',
+            'navigation-widgets',
+        ));
+
+
+         // Add support for editor styles
+        add_theme_support('editor-styles');
+        add_editor_style('style.css');
     }
 }
