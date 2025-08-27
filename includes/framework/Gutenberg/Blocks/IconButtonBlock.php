2e5bdd31ac5dd51bcee08d0cdaf588f176b05e9a
Puleeno Nguyen <puleeno@gmail.com>
2025-08-28 01:31:28 +0700

    fix icon button block

--- includes/framework/Gutenberg/Blocks/IconButtonBlock.php
+++ includes/framework/Gutenberg/Blocks/IconButtonBlock.php
@@ -1,10 +1,35 @@
 <?php
 
-namespace Jankx\Framework\Gutenberg\Blocks;
+namespace Jankx\Gutenberg\Blocks;
+
+use Jankx\Gutenberg\Block;
 
 class IconButtonBlock extends Block
 {
-    protected $blockName = 'jankx/icon-button';
+    public function __construct()
+    {
+        parent::__construct('jankx/icon-button', [
+            'title' => __('Icon Button', 'jankx'),
+            'category' => 'widgets',
+            'icon' => 'button',
+            'description' => __('Create an icon button', 'jankx'),
+            'keywords' => ['button', 'icon', 'click'],
+            'supports' => [
+                'html' => false,
+                'align' => ['left', 'center', 'right'],
+                'spacing' => [
+                    'margin' => true,
+                    'padding' => true
+                ]
+            ],
+            'attributes' => [
+                'className' => [
+                    'type' => 'string',
+                    'default' => ''
+                ]
+            ]
+        ]);
+    }
 
     public function register()
     {
@@ -12,5 +37,11 @@ class IconButtonBlock extends Block
         $metadata = $this->getBlockMetadata($blockPath);
 
         $this->registerBlock($blockPath, $metadata);
+    }
+
+    public function render($attributes, $content = '')
+    {
+        // Basic render implementation - can be enhanced later
+        return '<div class="icon-button-block">Icon Button Block</div>';
     }
 }
