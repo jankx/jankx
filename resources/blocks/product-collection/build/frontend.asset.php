bc7e87bae8909bfa6f15ce7b7ccd95103849f74f
Puleeno Nguyen <puleeno@gmail.com>
2025-09-02 19:42:04 +0700

    Clean scss

--- resources/blocks/product-collection/build/frontend.asset.php
+++ resources/blocks/product-collection/build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-interactivity', 'wp-interactivity-router'), 'version' => '60c66be1a5cd282d691a');
+<?php return array('dependencies' => array('wp-interactivity', 'wp-interactivity-router'), 'version' => '9220f23e465d668f7615');
