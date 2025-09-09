745db520ded8d731aed09f4577d8c8c370e11513
Puleeno Nguyen <puleeno@gmail.com>
2025-09-09 23:04:26 +0700

    Delete product-collection block

--- includes/framework/Services/GutenbergService.php
+++ includes/framework/Services/GutenbergService.php
@@ -9,7 +9,6 @@ use Jankx\Gutenberg\Blocks\IconPickerBlock;
 use Jankx\Gutenberg\Blocks\IconButtonBlock;
 use Jankx\Gutenberg\Blocks\LanguageSwitcherBlock;
 use Jankx\Gutenberg\Blocks\MegaMenuBlock;
-use Jankx\Gutenberg\Blocks\ProductCollectionBlock;
 use Jankx\Gutenberg\Blocks\SvgIconBlock;
 use Jankx\Gutenberg\Blocks\SvgIconButtonBlock;
 use Jankx\Gutenberg\Blocks\ImageButtonBlock;
@@ -116,7 +115,6 @@ class GutenbergService
         $this->repository->registerBlock(LanguageSwitcherBlock::class);
         $this->repository->registerBlock(WplyrMediaBlock::class);
         $this->repository->registerBlock(ImageButtonBlock::class);
-        $this->repository->registerBlock(ProductCollectionBlock::class);
     }
 
     /**
--- resources/blocks/product-collection/build/frontend.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-interactivity', 'wp-interactivity-router'), 'version' => 'f088acc6a97620933b31');
