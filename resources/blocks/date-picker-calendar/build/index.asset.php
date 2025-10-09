8be9bd8b2f656f675d39aa481562fd98b89f32d2
Puleeno Nguyen <puleeno@gmail.com>
2025-10-09 16:08:43 +0700

    Improve language switcher stylesheet

--- resources/blocks/date-picker-calendar/build/index.asset.php
+++ resources/blocks/date-picker-calendar/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '56fb3e307951725a5e66');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'cf00ba0eb7248dfe2752');
