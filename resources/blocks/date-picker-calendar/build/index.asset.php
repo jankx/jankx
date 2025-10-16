77e4cf1de2cd350be74222f3e20bcb2411618dfb
Puleeno Nguyen <puleeno@gmail.com>
2025-10-16 21:19:23 +0700

    follow primary color

--- resources/blocks/date-picker-calendar/build/index.asset.php
+++ resources/blocks/date-picker-calendar/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7affb8488399ef688009');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'aebb526594884aae0f3f');
