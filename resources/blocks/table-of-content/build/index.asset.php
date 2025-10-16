77e6efa8978c5b61dfcf296b30dd35ecccd47b0b
Puleeno Nguyen <puleeno@gmail.com>
2025-10-16 22:49:32 +0700

    Clean up table of content block

--- resources/blocks/table-of-content/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-server-side-render'), 'version' => 'f6c18aedea1b07c0d2bf');
