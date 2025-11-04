caa84876a3a06973b89680547443141b1085c13a
Puleeno Nguyen <puleeno@gmail.com>
2025-11-04 16:03:16 +0700

    update style for slideshow block

--- resources/blocks/slideshow/build/index.asset.php
+++ resources/blocks/slideshow/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '65b4199ec63007af2d5d');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '6809b6d50dbf6710b2dc');
