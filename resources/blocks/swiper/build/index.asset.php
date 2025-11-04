922c03093d6b06bdf82d5c5f895c1efaff29c52b
Puleeno Nguyen <puleeno@gmail.com>
2025-11-04 16:48:02 +0700

    Create swiper variations

--- resources/blocks/swiper/build/index.asset.php
+++ resources/blocks/swiper/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5d06fe8968a8bb1cdb81');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6bb10fe8ea627e58f71d');
