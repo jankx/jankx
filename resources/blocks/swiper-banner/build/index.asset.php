24d67ee30e790ff95efa5e4fcf00ae7d583064bf
Puleeno Nguyen <puleeno@gmail.com>
2025-11-05 11:51:36 +0700

    fix swiper banner height

--- resources/blocks/swiper-banner/build/index.asset.php
+++ resources/blocks/swiper-banner/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '87341cc57167e7c4c92e');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '9a89db614c30f2a5a007');
