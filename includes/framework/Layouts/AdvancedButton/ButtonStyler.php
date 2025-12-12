e211deb555318b72a591c7c9925d30a3865848fb
Puleeno Nguyen <puleeno@gmail.com>
2025-12-13 04:52:41 +0700

    create button stylers

---
+++ includes/framework/Layouts/AdvancedButton/ButtonStyler.php
@@ -0,0 +1,183 @@
+<?php
+
+namespace Jankx\Layouts\AdvancedButton;
+
+class ButtonStyler
+{
+    public static function hasBackgroundColor(array $attributes, string $content): bool
+    {
+        if (!empty($attributes['backgroundColor']) ||
+            !empty($attributes['gradient']) ||
+            !empty($attributes['style']['color']['background']) ||
+            !empty($attributes['style']['color']['gradient'])
+        ) {
+            return true;
+        }
+
+        return (
+            preg_match('/has-[a-z0-9\-]+-background-color/', $content) ||
+            preg_match('/has-[a-z0-9\-]+-gradient-background/', $content) ||
+            preg_match('/background-color\s*:\s*[^;]+/', $content) ||
+            preg_match('/background\s*:\s*[^;]*gradient/', $content)
+        );
+    }
+
+    public static function isOutlineMode(array $classes): bool
+    {
+        return in_array('is-style-outline', $classes);
+    }
+
+    public static function applyDefaultColors(
+        string $content,
+        array $attributes,
+        bool $isOutlineMode,
+        bool $hasBackgroundColor
+    ): string {
+        if ($hasBackgroundColor) {
+            return $content;
+        }
+
+        $buttonElement = ContentExtractor::getButtonElement($content);
+        if (!$buttonElement) {
+            return $content;
+        }
+
+        $additionalClasses = $isOutlineMode
+            ? 'has-primary-color has-base-color'
+            : 'has-primary-background-color has-contrast-color has-base-color';
+
+        $newButtonClass = str_replace(
+            'class="jankx-advanced-button__link',
+            'class="jankx-advanced-button__link ' . $additionalClasses,
+            $buttonElement['class_attr']
+        );
+
+        return str_replace($buttonElement['class_attr'], $newButtonClass, $content);
+    }
+
+    public static function getBorderRadius(array $attributes, string $content): ?string
+    {
+        if (!empty($attributes['style']['border']['radius'])) {
+            return $attributes['style']['border']['radius'];
+        }
+
+        if (preg_match('/style\s*=\s*["\'][^"\']*border-radius\s*:\s*([^;]+)/i', $content, $matches)) {
+            return trim($matches[1]);
+        }
+
+        return null;
+    }
+
+    public static function applyBorderRadius(string $content, string $borderRadius): string
+    {
+        if (empty($borderRadius)) {
+            return $content;
+        }
+
+        $buttonElement = ContentExtractor::getButtonElement($content);
+        if (!$buttonElement) {
+            return $content;
+        }
+
+        $buttonAttrs = $buttonElement['other_attrs'];
+
+        if (preg_match('/style\s*=\s*["\']([^"\']*)["\']/', $buttonAttrs, $styleMatches)) {
+            $existingStyles = $styleMatches[1];
+            $existingStyles = preg_replace('/border-radius\s*:\s*[^;]+;?/i', '', $existingStyles);
+            $existingStyles = trim($existingStyles, '; ');
+            $newStyles = $existingStyles;
+            if (!empty($newStyles)) {
+                $newStyles .= '; ';
+            }
+            $newStyles .= 'border-radius: ' . esc_attr($borderRadius);
+
+            $newButtonAttrs = preg_replace(
+                '/style\s*=\s*["\'][^"\']*["\']/',
+                'style="' . esc_attr($newStyles) . '"',
+                $buttonAttrs
+            );
+
+            $newButtonTag = '<' . $buttonElement['tag'] . $buttonElement['class_attr'] . ' ' . $newButtonAttrs . '>';
+            return str_replace($buttonElement['full'], $newButtonTag, $content);
+        } else {
+            $newButtonAttrs = 'style="border-radius: ' . esc_attr($borderRadius) . ';"';
+            if (!empty(trim($buttonAttrs))) {
+                $newButtonAttrs = ' ' . $newButtonAttrs;
+            }
+            $newButtonTag = '<' . $buttonElement['tag'] . $buttonElement['class_attr'] . $newButtonAttrs . $buttonAttrs . '>';
+            return str_replace($buttonElement['full'], $newButtonTag, $content);
+        }
+    }
+
+    public static function buildButtonClasses(array $attributes, string $baseClass = 'jankx-advanced-button__link'): string
+    {
+        $classes = [$baseClass];
+
+        $backgroundColor = $attributes['backgroundColor'] ?? null;
+        $textColor = $attributes['textColor'] ?? null;
+        $gradient = $attributes['gradient'] ?? null;
+        $iconPosition = $attributes['iconPosition'] ?? 'left';
+
+        if ($backgroundColor && isset($backgroundColor['slug'])) {
+            $classes[] = "has-{$backgroundColor['slug']}-background-color";
+        }
+
+        if ($textColor && isset($textColor['slug'])) {
+            $classes[] = "has-{$textColor['slug']}-color";
+        }
+
+        if ($gradient && isset($gradient['slug'])) {
+            $classes[] = "has-{$gradient['slug']}-gradient-background";
+        }
+
+        if (!empty($attributes['useIconBlocks']) && $iconPosition) {
+            $classes[] = "icon-position-{$iconPosition}";
+        }
+
+        $hasNoColorSettings = !$backgroundColor &&
+            !$textColor &&
+            !$gradient &&
+            empty($attributes['style']['color']['background']) &&
+            empty($attributes['style']['color']['text']) &&
+            empty($attributes['style']['color']['gradient']);
+
+        if ($hasNoColorSettings) {
+            $classes[] = 'has-base-color';
+        }
+
+        if (!empty($attributes['style']['color']['background']) || !empty($attributes['style']['color']['gradient'])) {
+            $classes[] = 'has-background';
+        }
+
+        if (!empty($attributes['style']['color']['text'])) {
+            $classes[] = 'has-text-color';
+        }
+
+        return implode(' ', $classes);
+    }
+
+    public static function buildButtonStyles(array $attributes): array
+    {
+        $styles = [];
+
+        if (!empty($attributes['style']['border']['radius'])) {
+            $styles['border-radius'] = $attributes['style']['border']['radius'];
+        }
+
+        if (!empty($attributes['style']['color']['background'])) {
+            $styles['background-color'] = $attributes['style']['color']['background'];
+        }
+
+        if (!empty($attributes['style']['color']['text'])) {
+            $styles['color'] = $attributes['style']['color']['text'];
+        }
+
+        if (!empty($attributes['style']['color']['gradient'])) {
+            $styles['background'] = $attributes['style']['color']['gradient'];
+            unset($styles['background-color']);
+        }
+
+        return $styles;
+    }
+}
+
