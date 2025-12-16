91093df86d70f1a6e151f4ccdf418871cdfff981
Puleeno Nguyen <puleeno@gmail.com>
2025-12-16 15:54:25 +0700

    feat(swiper): add banner style and improve gradient overlay
    - Add is-style-banner variation with auto height and min-height
    - Fix gradient overlay interaction by preventing hover opacity changes
    - Ensure proper containment of overlay effects with overflow hidden

--- resources/blocks/swiper/build/index.asset.php
+++ resources/blocks/swiper/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '6963d868a1a4824d25b0');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'bb9afa6d368f86c04437');
