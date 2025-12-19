e6e0545ba44317be09183844a5e86c6a4940f0e0
Puleeno Nguyen <puleeno@gmail.com>
2025-12-19 09:25:24 +0700

    micDataLayout): update block name check for ssr generator
    The block name was changed from 'jankx/dynamic-data-ssr' to
    'jankx/dynamic-ssr-template' to match the updated naming convention

--- includes/framework/Layouts/DynamicDataLayout/Renderer.php
+++ includes/framework/Layouts/DynamicDataLayout/Renderer.php
@@ -79,7 +79,7 @@ class Renderer
 
         if ($templateBlock) {
             $blockName = $templateBlock['blockName'] ?? '';
-            $generator = ($blockName === 'jankx/dynamic-data-ssr')
+            $generator = ($blockName === 'jankx/dynamic-ssr-template')
                 ? new SsrViewGenerator($templateBlock, $attributes)
                 : new PostTemplateBlockGenerator($templateBlock, $attributes);
             $layoutInstance = $decorator->getLayout();
