4a50aaec0c779d179b418dbacc0d0a781e9c1ebf
Puleeno Nguyen <puleeno@gmail.com>
2025-12-20 16:46:22 +0700

    feat(gallery): add metabox for managing gallery images
    - Add new metabox for selected post types to manage gallery images
    - Include JavaScript and CSS assets for gallery management UI
    - Update gallery retrieval logic to check for metabox images first

--- resources/blocks/facebook-page/build/index.asset.php
+++ resources/blocks/facebook-page/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '5f214e1548be7ad04e2f');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c7833b423e9850261585');
