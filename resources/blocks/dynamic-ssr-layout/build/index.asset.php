298aa6eb7dc5bb0665920164ef0a0d7dccabf46f
Puleeno Nguyen <puleeno@gmail.com>
2025-12-23 03:30:15 +0700

    feat(dynamic-ssr): add multi-post type support and query parameters
    Add useMultiPostType and postTypes context to enable multi-post type queries
    Include additional query parameters like taxQuery, metaQuery, and ordering options
    Update build files to reflect the new functionality

--- resources/blocks/dynamic-ssr-layout/build/index.asset.php
+++ resources/blocks/dynamic-ssr-layout/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '778b12118131b83c5e05');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'cfc326f698d9beb7f2dc');
--- resources/blocks/dynamic-ssr-template/build/index.asset.php
+++ resources/blocks/dynamic-ssr-template/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f0e9a886327513d96690');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7538f21276a08d892cd5');
