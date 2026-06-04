dd3c8cd2534dd9bb4115a43aef7971e268847259
Puleeno Nguyen <puleeno@gmail.com>
2026-06-04 23:07:29 +0700

    feat(dynamic-data-template): add new scroll reveal and animation options
    Add new rotate, flip, and slide direction scroll reveal animations, expand the available animation options for both scroll reveal and hover/unhover effects in the block editor, and compile updated styles and asset files.

--- resources/blocks/dynamic-data-template/build/index.asset.php
+++ resources/blocks/dynamic-data-template/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '34913fd085d3a564faab');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '91a596547e3306806496');
