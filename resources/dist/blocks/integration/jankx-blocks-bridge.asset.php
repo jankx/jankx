94c9b07e1de4ace2a9693114bbb07a997b063213
Puleeno Nguyen <puleeno@gmail.com>
2026-08-24 16:23:44 +0700

    build: generate production assets and dependency manifests for theme blocks

--- resources/dist/blocks/integration/jankx-blocks-bridge.asset.php
+++ resources/dist/blocks/integration/jankx-blocks-bridge.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-compose', 'wp-element', 'wp-hooks'), 'version' => '7f84c903085b37a604c6');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-compose', 'wp-element', 'wp-hooks'), 'version' => 'aa596523d12e27759fb9');
