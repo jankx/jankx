29421a05aac38d9adc6a86345b1f2378067a1ad5
Puleeno Nguyen <puleeno@gmail.com>
2026-09-02 07:06:38 +0700

    Fix user menu avatar border

--- resources/dist/blocks/user-menu/index.asset.php
+++ resources/dist/blocks/user-menu/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'f34d66e974ebd4b3013b');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '2e1fb7df19f4e249b64c');
