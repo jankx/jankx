9d49ccd372485f7dab73f6ab054335a2400fd7a3
Puleeno Nguyen <puleeno@gmail.com>
2026-09-20 11:30:15 +0700

    Add overlay styles for dynamic data template block
    - Introduced a new overlay class for the dynamic data template to enhance visual presentation.
    - Added styles for overlay links to improve user interaction with hover effects.
    - Updated the CSS source map to reflect the new styles.

--- resources/dist/blocks/dynamic-data-template/index.asset.php
+++ resources/dist/blocks/dynamic-data-template/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'd35d621fe8a03a30a4df');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'c48b714c54ad0eddff53');
