4530b36661142350a281bbe513772f8f160c7a9d
Puleeno Nguyen <puleeno@gmail.com>
2026-09-20 20:58:11 +0700

    Refactor code structure for improved readability and maintainability

--- resources/dist/blocks/dynamic-term-template/index.asset.php
+++ resources/dist/blocks/dynamic-term-template/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '6852fda2de5c3e0026fa');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '2c6b011fe20091c8f569');
