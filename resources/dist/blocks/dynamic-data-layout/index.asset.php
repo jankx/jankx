dc6747a5ad8dccfb3f53609996516659f084e448
Puleeno Nguyen <puleeno@gmail.com>
2026-09-24 07:54:39 +0700

    Refactor code structure for improved readability and maintainability

--- resources/dist/blocks/dynamic-data-layout/index.asset.php
+++ resources/dist/blocks/dynamic-data-layout/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ee5007302628d34e72e8');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '32bd804e79acd5ffcf4d');
