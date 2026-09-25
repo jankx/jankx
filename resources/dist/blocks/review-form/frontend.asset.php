ab1ffd4d78d9ebb9f28f3dee76e16e7960be7342
Puleeno Nguyen <puleeno@gmail.com>
2026-09-25 13:35:15 +0700

    Remove unused CSS and source map for review form block

--- resources/dist/blocks/review-form/frontend.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'b1a1b8cf5007236cd625');
--- resources/dist/blocks/review-form/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'de1e491ae8c9c7e33b7e');
